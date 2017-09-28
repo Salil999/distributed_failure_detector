ead75be04ab533770647c9501911f3f93bb7cb01
Salil999 <ssaxen4@illinois.edu>
2017-09-28 17:34:48 -0500

    testing out start scripts

---
+++ scripts/start_client.sh
@@ -0,0 +1,4 @@
+cd /home/ssaxen4/ECE428_mp2/src
+rm ece428/mp1/*.class
+javac -cp . ece428/mp1/*.java
+java -cp . ece428/mp1/StartClient
---
+++ src/ece428/mp1/StartClient.java
@@ -0,0 +1,7 @@
+package ece428.mp1;
+
+public class StartClient {
+    public static void main(final String[] args) {
+        System.out.println("This is running");
+    }
+}
