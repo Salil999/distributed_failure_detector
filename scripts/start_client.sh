35ff061e89014cc731a4517b92e6276a8c1359db
Salil999 <ssaxen4@illinois.edu>
2017-10-03 04:18:31 -0500

    testing

--- scripts/start_client.sh
+++ scripts/start_client.sh
@@ -1,12 +1,4 @@
-read user
-
-if [[ $user = r ]]; then
-	net_id = "rsurti2"
-elif [[ $user = s ]]; then
-	net_id = "ssaxen4"
-fi
-
-cd /home/$net_id/ECE428_mp2/src
+cd /home/$whoami/ECE428_mp2/src
 rm ece428/mp1/*.class
 javac -cp . ece428/mp1/*.java
 java -cp . ece428/mp1/Main
