4070fd2b1937b1efa0b5c333001d994b2e4878dd
Salil999 <ssaxen4@illinois.edu>
2017-10-04 22:35:46 -0500

    testing

--- src/ece428/mp1/Servent.java
+++ src/ece428/mp1/Servent.java
@@ -82,23 +82,29 @@ public class Servent {
                     System.out.println("client-looping");
                     final byte[] incomingByteStream = new byte[1024];
                     final int port = Servent.this.connection.getPort() + 1;
+
                     Servent.this.socketClient = new DatagramSocket(
                             port,
                             Servent.this.connection.getHost()
                     );
+
                     final byte[] data = "This is a message from client".getBytes();
 
                     final DatagramPacket sendPacket = new DatagramPacket(
                             data, data.length,
-                            Servent.this.connection.getHost(), port
+                            Servent.this.connection.getHost(), Servent.this.connection.getPort()
                     );
+
                     Servent.this.socketClient.send(sendPacket);
+
                     final DatagramPacket incomingPacket = new DatagramPacket(
                             incomingByteStream, incomingByteStream.length
                     );
-                    Servent.this.socketClient.receive(incomingPacket);
-                    System.out.println("Message from client: " + new String(incomingPacket.getData()));
-                    Servent.this.socketClient.close();
+
+//                    Servent.this.socketClient.receive(incomingPacket);
+//
+//                    System.out.println("Message from client: " + new String(incomingPacket.getData()));
+//                    Servent.this.socketClient.close();
                 } catch (final IOException e) {
                     System.out.println(e.getLocalizedMessage());
                     e.printStackTrace();
