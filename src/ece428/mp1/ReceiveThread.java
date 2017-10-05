fd0336a145e279142382fd17765f56982f527c78
Salil999 <ssaxen4@illinois.edu>
2017-10-04 19:03:15 -0500

    testing

--- src/ece428/mp1/ReceiveThread.java
+++ src/ece428/mp1/ReceiveThread.java
@@ -1,51 +1,51 @@
-package ece428.mp1;
-
-import java.io.BufferedReader;
-import java.io.FileReader;
-import java.io.IOException;
-import java.net.DatagramPacket;
-import java.net.DatagramSocket;
-import java.net.InetAddress;
-import java.util.concurrent.Callable;
-
-public class ReceiveThread implements Callable {
-    final BufferedReader bufferedReader = new BufferedReader(new FileReader("../number.txt"));
-    private final Connection connection;
-    Integer number = Integer.parseInt(this.bufferedReader.readLine());
-
-    public ReceiveThread() throws IOException {
-        System.out.println(this.number);
-        this.connection = new Connection(
-                InetAddress.getByName("fa17-cs425-g39-0" + this.number + ".cs.illinois.edu"),
-                9090,
-                new DatagramSocket(9090),
-                new byte[4096]
-        );
-    }
-
-    public ReceiveThread(final byte[] byteStream) throws IOException {
-        this.connection = new Connection(
-                InetAddress.getByName("fa17-cs425-g39-0" + this.number + ".cs.illinois.edu"),
-                9090,
-                new DatagramSocket(9090),
-                byteStream
-        );
-    }
-
-    @Override
-    public Object call() throws IOException {
-        DatagramPacket receivePacket;
-        String membershipList;
-        System.out.println("ReceiveThread: " + this.connection.getHost());
-        while (true) {
-            // change this to membership list
-            receivePacket = new DatagramPacket(this.connection.getByteStream(), this.connection.getByteStream().length);
-
-            this.connection.getDatagramSocket().receive(receivePacket);
-
-            membershipList = new String(receivePacket.getData());
-
-            System.out.println("FROM SERVER:" + membershipList);
-        }
-    }
-}
+//package ece428.mp1;
+//
+//import java.io.BufferedReader;
+//import java.io.FileReader;
+//import java.io.IOException;
+//import java.net.DatagramPacket;
+//import java.net.DatagramSocket;
+//import java.net.InetAddress;
+//import java.util.concurrent.Callable;
+//
+//public class ReceiveThread implements Callable {
+//    final BufferedReader bufferedReader = new BufferedReader(new FileReader("../number.txt"));
+//    private final Connection connection;
+//    Integer number = Integer.parseInt(this.bufferedReader.readLine());
+//
+//    public ReceiveThread() throws IOException {
+//        System.out.println(this.number);
+//        this.connection = new Connection(
+//                InetAddress.getByName("fa17-cs425-g39-0" + this.number + ".cs.illinois.edu"),
+//                9090,
+//                new DatagramSocket(9090),
+//                new byte[4096]
+//        );
+//    }
+//
+//    public ReceiveThread(final byte[] byteStream) throws IOException {
+//        this.connection = new Connection(
+//                InetAddress.getByName("fa17-cs425-g39-0" + this.number + ".cs.illinois.edu"),
+//                9090,
+//                new DatagramSocket(9090),
+//                byteStream
+//        );
+//    }
+//
+//    @Override
+//    public Object call() throws IOException {
+//        DatagramPacket receivePacket;
+//        String membershipList;
+//        System.out.println("ReceiveThread: " + this.connection.getHost());
+//        while (true) {
+//            // change this to membership list
+//            receivePacket = new DatagramPacket(this.connection.getByteStream(), this.connection.getByteStream().length);
+//
+//            this.connection.getDatagramSocket().receive(receivePacket);
+//
+//            membershipList = new String(receivePacket.getData());
+//
+//            System.out.println("FROM SERVER:" + membershipList);
+//        }
+//    }
+//}
--- src/ece428/mp1/SendThread.java
+++ src/ece428/mp1/SendThread.java
@@ -1,60 +1,60 @@
-package ece428.mp1;
-
-import java.io.BufferedReader;
-import java.io.FileReader;
-import java.io.IOException;
-import java.net.DatagramPacket;
-import java.net.DatagramSocket;
-import java.net.InetAddress;
-import java.util.concurrent.Callable;
-
-public class SendThread implements Callable {
-    final BufferedReader bufferedReader = new BufferedReader(new FileReader("../number.txt"));
-    private final Connection connection;
-    Integer number = Integer.parseInt(this.bufferedReader.readLine());
-
-    public SendThread() throws IOException {
-        if (this.number == 1) {
-            this.number = 2;
-        } else {
-            this.number = 1;
-        }
-        System.out.println(this.number);
-        this.connection = new Connection(
-                InetAddress.getByName("fa17-cs425-g39-0" + this.number + ".cs.illinois.edu"),
-                9090,
-                new DatagramSocket(9090),
-                new byte[4096]
-        );
-    }
-
-    public SendThread(final byte[] byteStream) throws IOException {
-        this.connection = new Connection(
-                InetAddress.getByName("fa17-cs425-g39-0" + this.number + ".cs.illinois.edu"),
-                9090,
-                new DatagramSocket(9090),
-                new byte[4096]
-        );
-    }
-
-    @Override
-    public Object call() throws IOException, InterruptedException {
-        System.out.println("SendThread: " + this.connection.getHost());
-        while (true) {
-            Thread.sleep(500);
-            System.out.println("Sending packet...");
-            // change this to membership list
-            final String capitalizedSentence = "send_packet_test";
-            this.connection.setByteStream(capitalizedSentence.getBytes());
-
-            // for each of the monitors monitoring heartbeats, get ip and port
-            final DatagramPacket sendPacket = new DatagramPacket(
-                    this.connection.getByteStream(),
-                    this.connection.getByteStream().length,
-                    this.connection.getHost(),
-                    this.connection.getPort());
-
-            this.connection.getDatagramSocket().send(sendPacket);
-        }
-    }
-}
+//package ece428.mp1;
+//
+//import java.io.BufferedReader;
+//import java.io.FileReader;
+//import java.io.IOException;
+//import java.net.DatagramPacket;
+//import java.net.DatagramSocket;
+//import java.net.InetAddress;
+//import java.util.concurrent.Callable;
+//
+//public class SendThread implements Callable {
+//    final BufferedReader bufferedReader = new BufferedReader(new FileReader("../number.txt"));
+//    private final Connection connection;
+//    Integer number = Integer.parseInt(this.bufferedReader.readLine());
+//
+//    public SendThread() throws IOException {
+//        if (this.number == 1) {
+//            this.number = 2;
+//        } else {
+//            this.number = 1;
+//        }
+//        System.out.println(this.number);
+//        this.connection = new Connection(
+//                InetAddress.getByName("fa17-cs425-g39-0" + this.number + ".cs.illinois.edu"),
+//                9090,
+//                new DatagramSocket(9090),
+//                new byte[4096]
+//        );
+//    }
+//
+//    public SendThread(final byte[] byteStream) throws IOException {
+//        this.connection = new Connection(
+//                InetAddress.getByName("fa17-cs425-g39-0" + this.number + ".cs.illinois.edu"),
+//                9090,
+//                new DatagramSocket(9090),
+//                new byte[4096]
+//        );
+//    }
+//
+//    @Override
+//    public Object call() throws IOException, InterruptedException {
+//        System.out.println("SendThread: " + this.connection.getHost());
+//        while (true) {
+//            Thread.sleep(500);
+//            System.out.println("Sending packet...");
+//            // change this to membership list
+//            final String capitalizedSentence = "send_packet_test";
+//            this.connection.setByteStream(capitalizedSentence.getBytes());
+//
+//            // for each of the monitors monitoring heartbeats, get ip and port
+//            final DatagramPacket sendPacket = new DatagramPacket(
+//                    this.connection.getByteStream(),
+//                    this.connection.getByteStream().length,
+//                    this.connection.getHost(),
+//                    this.connection.getPort());
+//
+//            this.connection.getDatagramSocket().send(sendPacket);
+//        }
+//    }
+//}
