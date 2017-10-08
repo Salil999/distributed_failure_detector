8748b98b29f42da2ede7af76d4526ef48a0cf39a
Salil999 <ssaxen4@illinois.edu>
2017-10-08 17:36:09 -0500

    testing

--- src/ece428/mp1/MembershipListEntry.java
+++ src/ece428/mp1/MembershipListEntry.java
@@ -22,7 +22,7 @@ public class MembershipListEntry {
 
     public MembershipListEntry(final int heartBeatCounter, final long localTime, final boolean isAlive) {
         this.heartBeatCounter = heartBeatCounter;
-        this.localTime = LocalDateTime.now().atZone(ZoneId.systemDefault()).toInstant().toEpochMilli();
+        this.localTime = localTime;
         this.isAlive = true;
     }
 
