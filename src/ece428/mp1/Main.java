03ae4dcdb7ee8e747546c6875de71e81fccb1e6d
Salil999 <ssaxen4@illinois.edu>
2017-10-07 23:36:36 -0500

    testing

--- src/ece428/mp1/Main.java
+++ src/ece428/mp1/Main.java
@@ -1,16 +1,19 @@
 package ece428.mp1;
 
+import java.io.BufferedReader;
+import java.io.FileReader;
+
 public class Main {
 
 
     public static void main(final String[] args) throws Exception {
-//        final Integer machineNumber = Integer.parseInt(new BufferedReader(new FileReader("../number.txt")).readLine());
-//        if (machineNumber == 1) {
-//            final Introducer introducer = new Introducer();
-//            introducer.startServent();
-//        } else {
-        final Servent servent = new Servent();
-        servent.startServent();
-//        }
+        final Integer machineNumber = Integer.parseInt(new BufferedReader(new FileReader("../number.txt")).readLine());
+        if (machineNumber == 1) {
+            final Introducer introducer = new Introducer();
+            introducer.startServent();
+        } else {
+            final Servent servent = new Servent();
+            servent.startServent();
+        }
     }
 }
