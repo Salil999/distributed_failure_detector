92bb91c43d3c242a3085e4d4a783a945c648e069
Salil999 <ssaxen4@illinois.edu>
2017-10-08 04:50:19 -0500

    testing

--- src/ece428/mp1/MembershipListEntry.java
+++ src/ece428/mp1/MembershipListEntry.java
@@ -75,7 +75,7 @@ public class MembershipListEntry {
             this.setAlive(true);
             this.setFailedTime(-1);
         } else if ((!other.getAlive() || shouldKill) && this.getFailedTime() < 0) {
-            System.out.println("killing " + nodeID.getIPAddress().getHostName());
+//            System.out.println("killing " + nodeID.getIPAddress().getHostName());
             this.setAlive(false);
             this.setFailedTime(LocalDateTime.now().atZone(ZoneId.systemDefault()).toInstant().toEpochMilli());
         }
